dbc69879cc2ac83dd15541b2c29360a7c9722ec8
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-04-20 16:13:08 -0500

    connecting to database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +8,18 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your My SQL password: '',
+    password: '4thBestCountryBand',
+    database: 'election'
+  },
+);
 
 /*
 app.get('/', (req, res) => {
